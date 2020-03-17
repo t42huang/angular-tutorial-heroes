85fb6ca3c5a768e3df3739300b4ba2adb87a1a34
Tina Huang <t42huang@gmail.com>
2020-03-17 08:35:48 -0400

    use asynchronous approach to get the data to avoid UI freeze in real situation where the data could take some time to get back

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HEROES } from './heroes-mock';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
