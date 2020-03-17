7f783f80de30645b7729b7141aa80cd820f8149d
Tina Huang <t42huang@gmail.com>
2020-03-17 09:22:57 -0400

    inject service in another service for components - display message when data loaded

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
 import { HEROES } from './heroes-mock';
 import { Hero } from './hero';
-import { Observable, of } from 'rxjs';
+
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message after fethcing the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
