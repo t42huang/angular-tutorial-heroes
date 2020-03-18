af0ebcfe78b13fe661278bd42df4ebccd6478e7d
Tina Huang <t42huang@gmail.com>
2020-03-17 23:05:05 -0400

    get hero by id from the in-memory web api, implemented with catchError and error handler, and tap to log the action

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,7 +9,7 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 11, name: 'Dr Nicely' },
-      { id: 12, name: 'Narco' },
+      { id: 12, name: 'Nacho' },
       { id: 13, name: 'Bombasto' },
       { id: 14, name: 'Celeritas' },
       { id: 15, name: 'Magneta' },
